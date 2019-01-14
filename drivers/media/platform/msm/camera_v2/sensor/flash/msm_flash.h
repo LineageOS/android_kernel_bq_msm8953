aefa149a337ba8d211c1bb3144fcc4eeaf272a67
Jenkins <jenkins@bqbot.bq.local>
2019-01-14 16:28:59 +0100

    drivers: camera: Add BQ flash updates. Version 2.5.0_20181227-1107 released
    Based on TAG LA.UM.6.6.r1-09900-89xx.0 from CAF.
    
    Change-Id: Ie9bc94bcbe3a3d1e0328209b43592c010829b6c9

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.h
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2016,2018-2019, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -31,6 +31,9 @@ enum msm_camera_flash_state_t {
 	MSM_CAMERA_FLASH_LOW,
 	MSM_CAMERA_FLASH_HIGH,
 	MSM_CAMERA_FLASH_RELEASE,
+	MSM_CAMERA_TORCH_HIGH,
+	MSM_CAMERA_TORCH_MID,
+	MSM_CAMERA_TORCH_LOW,
 };
 
 struct msm_flash_ctrl_t;
@@ -107,6 +110,7 @@ struct msm_flash_ctrl_t {
 	bool front_flash_init;
 	uint16_t front_gpio_torch;
 	uint16_t front_gpio_flash;
+	const char * front_gpio_torch_labs;
 	const char * front_gpio_flash_labs;
 	bool rear_flash_init;
 	uint16_t rear_gpio_torch;
