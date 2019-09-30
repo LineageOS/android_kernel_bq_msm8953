31eb478986d9f035e82c8a4db1d014eb315e43b5
Quallenauge <Hamsi2k@freenet.de>
2019-09-30 11:35:54 +0200

    Add camera drivers from 3.18

--- drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.h
+++ drivers/media/platform/msm/camera_v2/sensor/flash/msm_flash.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2009-2016, 2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -31,9 +31,6 @@ enum msm_camera_flash_state_t {
 	MSM_CAMERA_FLASH_LOW,
 	MSM_CAMERA_FLASH_HIGH,
 	MSM_CAMERA_FLASH_RELEASE,
-	MSM_CAMERA_TORCH_HIGH,
-	MSM_CAMERA_TORCH_MID,
-	MSM_CAMERA_TORCH_LOW,
 };
 
 struct msm_flash_ctrl_t;
@@ -47,14 +44,6 @@ struct msm_flash_func_t {
 	int32_t (*camera_flash_low)(struct msm_flash_ctrl_t *,
 		struct msm_flash_cfg_data_t *);
 	int32_t (*camera_flash_high)(struct msm_flash_ctrl_t *,
-		struct msm_flash_cfg_data_t *);
-	int32_t (*camera_flash_query_current)(struct msm_flash_ctrl_t *,
-		struct msm_flash_query_data_t *);
-	int32_t (*camera_flash_torch_low)(struct msm_flash_ctrl_t *,
-		struct msm_flash_cfg_data_t *);
-	int32_t (*camera_flash_torch_mid)(struct msm_flash_ctrl_t *,
-		struct msm_flash_cfg_data_t *);
-	int32_t (*camera_flash_torch_high)(struct msm_flash_ctrl_t *,
 		struct msm_flash_cfg_data_t *);
 };
 
@@ -81,7 +70,6 @@ struct msm_flash_ctrl_t {
 	/* Switch node to trigger led */
 	const char *switch_trigger_name;
 	struct led_trigger *switch_trigger;
-	uint32_t is_regulator_enabled;
 
 	/* Flash */
 	uint32_t flash_num_sources;
@@ -110,16 +98,6 @@ struct msm_flash_ctrl_t {
 
 	/* flash state */
 	enum msm_camera_flash_state_t flash_state;
-	bool front_flash_init;
-	uint16_t front_gpio_torch;
-	uint16_t front_gpio_flash;
-	const char * front_gpio_torch_labs;
-	const char * front_gpio_flash_labs;
-	bool rear_flash_init;
-	uint16_t rear_gpio_torch;
-	uint16_t rear_gpio_flash;
-	const char * rear_gpio_torch_labs;
-	const char * rear_gpio_flash_labs;
 };
 
 int msm_flash_i2c_probe(struct i2c_client *client,
